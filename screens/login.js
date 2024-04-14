0897c45be8cd056d8fceaa46acf6253f301c577e
OLEGAST212 <166819048+OLEGAST212@users.noreply.github.com>
2024-04-15 00:21:41 +0300

    feat
    Добавлена проверка логина и пароля в случае не совпадения вывод ошибки

--- screens/login.js
+++ screens/login.js
@@ -1,25 +1,57 @@
-// screens/login.js
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 
-import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(''); // Добавляем состояние для ошибки
 
-function Login() {
-    return (
-        <View style={styles.container}>
-            <TextInput placeholder="Логин" />
-            <TextInput placeholder="Пароль" secureTextEntry={true} />
-            <Button title="Войти" />
-        </View>
-    );
-}
+  const handleLoginPress = () => {
+    if (username === 'admin' && password === 'admin') {
+      onLogin(); // Вызываем onLogin, если учетные данные верны
+    } else {
+      setError('Неверный логин или пароль'); // Устанавливаем сообщение об ошибке
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Имя пользователя"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Пароль"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {error ? <Text style={styles.errorText}>{error}</Text> : null} // Отображаем ошибку, если она есть
+      <Button title="Войти" onPress={handleLoginPress} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    width: '80%',
+    padding: 10,
+  },
+  errorText: {
+    color: 'red', // Стиль для текста ошибки
+  },
 });
 
 export default Login;
